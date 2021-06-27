0ad56417f52d11d7f2856330bbd5b1af8fa3f0ed
elPaleniozord <jacekwalasik89@gmail.com>
2021-06-27 23:59:09 +0200

    implement modals

--- src/App.js
+++ src/App.js
@@ -1,20 +1,29 @@
+import { useState } from 'react';
 import './App.css';
-import Modal from './components/modal'
+import { Modal, ModalProvider } from './components/modal'
 import Level from './components/level'
 import Menu from './components/menu'
 import useOrientation from './hooks/useOrientation';
 
 function App() {
   const orientation = useOrientation()
+  const [modalOpen , setModalOpen] = useState(true)
+
   return (
-    <div className="App">
-      <header className="App-header">
-      </header>
+    <ModalProvider className="App">
+      {
+        modalOpen && (
+          <Modal onClose={() => setModalOpen(false)}>
+            <p>Welcome to </p>
+            <h1>Javascript Level</h1>
+            <div></div>
+          </Modal>
+        )
+      }
 
       <Level orientation={orientation.values} />
       <Menu orientation={orientation.values} calibrate={orientation.calibrate} offset={orientation.offset} />
-      <Modal />
-    </div>
+    </ModalProvider>
   );
 }
 
--- src/components/modal.js
+++ src/components/modal.js
@@ -1,14 +1,36 @@
-import React from 'react'
+import React, {useContext, useRef, useState, useEffect} from 'react'
+import ReactDOM from 'react-dom'
 
-const Modal = () => {
-  const handleClose = () => {
-    console.log('Closig modal')
-  }
+const Context = React.createContext()
+
+export const ModalProvider = ({children}) => {
+  const modalRef = useRef()
+  const [context, setContext] = useState()
+
+  useEffect(() => {
+    setContext(modalRef.current)
+  }, [])
+
   return (
-    <div>
-      <button onClick={handleClose}>Ok</button>
+    <div className='modal-container'>
+      <Context.Provider value={context}>{children}</Context.Provider>
+      <div ref={modalRef} />
     </div>
   )
 }
 
-export default Modal
+export const Modal = ({onClose, children, ...props}) => {
+  const modalNode = useContext(Context)
+
+  return modalNode ? 
+    ReactDOM.createPortal(
+      <div className='modal-overlay'>
+        <div className='modal-dialog'>
+          {children}
+          <button onClick={onClose}>Close</button>
+        </div>
+      </div>, modalNode
+    )
+    :
+    null
+}
